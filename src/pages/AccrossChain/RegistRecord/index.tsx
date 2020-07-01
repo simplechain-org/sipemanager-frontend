bdff35eb5816054906169f41667e0c67d56a9cb6
ql <ql@dataqin.com>
2020-07-01 18:08:40 +0800

    less fix

--- src/pages/AccrossChain/RegistRecord/index.tsx
+++ src/pages/AccrossChain/RegistRecord/index.tsx
@@ -1,10 +1,248 @@
 import React from 'react';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
+import { Table, Space, Button, Modal, Form, Input } from 'antd';
 
-export default function RegistRecord() {
-  return (
-    <PageHeaderWrapper>
-      <div>跨链注册记录</div>;
-    </PageHeaderWrapper>
-  );
+import Card from '../../../components/Card/Card';
+
+interface Pagination {
+  page: number;
+  pageSize: number;
+  total: number;
+}
+
+interface FormValues {
+  name: string;
+  address: string;
+  password: string;
+  userId?: number;
+}
+
+const dataSource = [
+  {
+    userId: 1,
+    name: '胡彦斌',
+    address: '西湖区湖底公园1号',
+    content: [1, 2, 3],
+  },
+  {
+    userId: 2,
+    name: '胡彦斌',
+    address: '西湖区湖底公园1号',
+    content: [1, 2, 3],
+  },
+  {
+    userId: 3,
+    name: '胡彦斌',
+    address: '西湖区湖底公园1号',
+    content: [1, 2, 3],
+  },
+  {
+    userId: 4,
+    name: '胡彦斌',
+    address: '西湖区湖底公园1号',
+    content: [1, 2, 3],
+  },
+  {
+    userId: 5,
+    name: '胡彦斌',
+    address: '西湖区湖底公园1号',
+    content: [1, 2, 3],
+  },
+  {
+    userId: 6,
+    name: '胡彦斌',
+    address: '西湖区湖底公园1号',
+    content: [1, 2, 3],
+  },
+  {
+    userId: 7,
+    name: '胡彦斌',
+    address: '西湖区湖底公园1号',
+    content: [1, 2, 3],
+  },
+  {
+    userId: 8,
+    name: '胡彦斌',
+    address: '西湖区湖底公园1号',
+    content: [1, 2, 3],
+  },
+  {
+    userId: 9,
+    name: '胡彦斌',
+    address: '西湖区湖底公园1号',
+    content: [1, 2, 3],
+  },
+  {
+    userId: 10,
+    name: '胡彦斌',
+    address: '西湖区湖底公园1号',
+    content: [1, 2, 3],
+  },
+];
+const initialState = {
+  addVisible: false,
+  loading: false,
+  data: [],
+  pagination: {
+    page: 1,
+    pageSize: 10,
+  },
+};
+
+const columns = [
+  {
+    title: '注册时间',
+    dataIndex: 'CreatedAt',
+    key: 'CreatedAt',
+  },
+  {
+    title: '发起链',
+    dataIndex: 'source_chain_id',
+    key: 'source_chain_id',
+  },
+  {
+    title: '目标链',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: '最低签名数',
+    dataIndex: 'confirm',
+    key: 'confirm',
+  },
+  {
+    title: '锚定节点数',
+    dataIndex: 'anchor_addresses',
+    key: 'anchor_addresses',
+  },
+  {
+    title: '注册TX哈希',
+    dataIndex: 'tx_hash',
+    key: 'tx_hash',
+  },
+  {
+    title: '操作',
+    key: 'action',
+    render: () => (
+      <Space>
+        <a>编辑</a>
+        <a>查看</a>
+      </Space>
+    ),
+  },
+];
+
+type IState = Readonly<typeof initialState>;
+
+export default class WalletManage extends React.Component<IState> {
+  readonly state: IState = initialState;
+
+  addHandle = (modalType: string) => {
+    console.log(modalType);
+    this.openModal(modalType);
+  };
+
+  tableChangeHandle = (newPagination: any) => {
+    console.log('newPagination', newPagination);
+  };
+
+  openModal = (modalType: string) => {
+    console.log(modalType);
+    this.setState({
+      [modalType]: true,
+    });
+  };
+
+  closeModal = (modalType: string) => {
+    console.log(modalType);
+    this.setState({
+      [modalType]: false,
+    });
+  };
+
+  handleOk = (modalType: string) => {
+    this.closeModal(modalType);
+  };
+
+  handleCancel = (modalType: string) => {
+    this.closeModal(modalType);
+  };
+
+  render() {
+    const { loading, addVisible, pagination } = this.state;
+    return (
+      <PageHeaderWrapper>
+        <div className="container">
+          <Card>
+            <Button
+              type="primary"
+              style={{ float: 'right' }}
+              onClick={() => this.addHandle('addVisible')}
+            >
+              新增
+            </Button>
+          </Card>
+          <Table
+            rowKey={(record) => record.userId}
+            dataSource={dataSource}
+            columns={columns}
+            pagination={pagination}
+            loading={loading}
+            onChange={() => this.tableChangeHandle(pagination)}
+            bordered
+          />
+          <Modal
+            centered
+            title="注册新的跨链对"
+            visible={addVisible}
+            onOk={() => this.handleOk('addVisible')}
+            onCancel={() => this.handleCancel('addVisible')}
+          >
+            <div className="modal_container">
+              <Form name="nest-messages" onFinish={(values) => this.handleOk('addVisible', values)}>
+                <Form.Item label="链A" name="name">
+                  <Input />
+                </Form.Item>
+                <Form.Item label="链A的节点" name="password">
+                  <Input type="password" />
+                </Form.Item>
+                <Form.Item label="链B" name="password">
+                  <Input type="password" />
+                </Form.Item>
+                <Form.Item label="链B的节点" name="password">
+                  <Input type="password" />
+                </Form.Item>
+                <Form.Item label="锚定节点地址1" name="name">
+                  <Input />
+                  {/* <Button>新增</Button> */}
+                </Form.Item>
+                <Form.Item label="地址1名称" name="password">
+                  <Input type="password" />
+                </Form.Item>
+                <Form.Item label="锚定节点地址2" name="password">
+                  <Input type="password" />
+                  {/* <Button>删除</Button> */}
+                </Form.Item>
+                <Form.Item label="地址2名称" name="password">
+                  <Input type="password" />
+                </Form.Item>
+                <Form.Item label="锚定节点质押金额" name="name">
+                  <Input />
+                </Form.Item>
+                <Form.Item label="最低签名数" name="password">
+                  <Input type="password" />
+                </Form.Item>
+                <Form.Item label="选择钱包账户" name="password">
+                  <Input type="password" />
+                </Form.Item>
+                <Form.Item label="钱包密码" name="password">
+                  <Input type="password" />
+                </Form.Item>
+              </Form>
+            </div>
+          </Modal>
+        </div>
+      </PageHeaderWrapper>
+    );
+  }
 }
--- src/pages/Wallet/WalletManage/index.tsx
+++ src/pages/Wallet/WalletManage/index.tsx
@@ -1,165 +1,291 @@
-// import React, { useState } from 'react';
 import React from 'react';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
-// import { Table, Space, Pagination } from 'antd';
-import { Table, Space } from 'antd';
+import { Table, Space, Button, Modal, Form, Input } from 'antd';
 
-// interface Pagination {
-//   page: number,
-//   pageSize: number,
-//   total: number
+import Card from '../../../components/Card/Card';
+
+// export default function WalletManage() {
+//   const [pagination, setPagination] = useState({page: 1, pageSize: 10, total: dataSource.length})
+//   const [loading, setLoading] = useState(false)
+//   const [visible, setVisible] = useState(false)
+
+//   const tableChangeHandle = (newPagination: Pagination) => {
+//     console.log('table changed ~', newPagination)
+//   }
+
+// const addHandle = () => {
+//   setVisible(true)
 // }
 
-// interface Item {
-//   key: string,
-//   name: string,
-//   age: number,
-//   address: string
+//   const handleOk = () => {
+//     setVisible(false)
+//   }
+
+//   const handleCancel = () => {
+//     setVisible(false)
+//   }
+
+// return (
+//   <PageHeaderWrapper>
+//     <div className="container">
+//       <Card>
+//         <Button type='primary' style={{float: 'right'}} onClick={addHandle}>新增</Button>
+//       </Card>
+//       <Table
+//         rowKey={record => record.userId}
+//         dataSource={dataSource}
+//         columns={columns}
+//         pagination={pagination}
+//         loading={loading}
+//         onChange={() => tableChangeHandle(pagination)}
+//         bordered
+//       />
+//       <Modal
+//         title='新增钱包'
+//         visible={visible}
+//         onOk={handleOk}
+//         onCancel={handleCancel}
+//       >
+//         <p>Some contents...</p>
+//       </Modal>
+//     </div>
+//   </PageHeaderWrapper>
+// );
+// }
+// interface IState {
+//   visible: boolean,
+//     loading: boolean,
+//     dataSource: Array<object>,
+//     pagination: {
+//       page: number,
+//       pageSize: number
+//     }
 // }
 
+interface Pagination {
+  page: number;
+  pageSize: number;
+  total: number;
+}
+
+interface FormValues {
+  name: string;
+  address: string;
+  password: string;
+  userId?: number;
+}
+
+// interface PropsType {
+//   form: Form;
+// }
 const dataSource = [
   {
-    key: '1',
+    userId: 1,
     name: '胡彦斌',
-    age: 32,
     address: '西湖区湖底公园1号',
+    content: [1, 2, 3],
   },
   {
-    key: '2',
-    name: '胡彦祖',
-    age: 42,
-    address: '西湖区湖底公园1号',
-  },
-  {
-    key: '3',
-    name: '胡彦祖',
-    age: 42,
-    address: '西湖区湖底公园1号',
-  },
-  {
-    key: '4',
+    userId: 2,
     name: '胡彦斌',
-    age: 32,
     address: '西湖区湖底公园1号',
+    content: [1, 2, 3],
   },
   {
-    key: '5',
-    name: '胡彦祖',
-    age: 42,
-    address: '西湖区湖底公园1号',
-  },
-  {
-    key: '6',
-    name: '胡彦祖',
-    age: 42,
-    address: '西湖区湖底公园1号',
-  },
-  {
-    key: '7',
+    userId: 3,
     name: '胡彦斌',
-    age: 32,
     address: '西湖区湖底公园1号',
+    content: [1, 2, 3],
   },
   {
-    key: '8',
-    name: '胡彦祖',
-    age: 42,
-    address: '西湖区湖底公园1号',
-  },
-  {
-    key: '9',
-    name: '胡彦祖',
-    age: 42,
-    address: '西湖区湖底公园1号',
-  },
-  {
-    key: '10',
+    userId: 4,
     name: '胡彦斌',
-    age: 32,
     address: '西湖区湖底公园1号',
+    content: [1, 2, 3],
   },
   {
-    key: '11',
-    name: '胡彦祖',
-    age: 42,
-    address: '西湖区湖底公园1号',
-  },
-  {
-    key: '12',
-    name: '胡彦祖',
-    age: 42,
-    address: '西湖区湖底公园1号',
-  },
-  {
-    key: '13',
+    userId: 5,
     name: '胡彦斌',
-    age: 32,
     address: '西湖区湖底公园1号',
+    content: [1, 2, 3],
   },
   {
-    key: '14',
-    name: '胡彦祖',
-    age: 42,
+    userId: 6,
+    name: '胡彦斌',
     address: '西湖区湖底公园1号',
+    content: [1, 2, 3],
   },
   {
-    key: '15',
-    name: '胡彦祖',
-    age: 42,
+    userId: 7,
+    name: '胡彦斌',
     address: '西湖区湖底公园1号',
+    content: [1, 2, 3],
+  },
+  {
+    userId: 8,
+    name: '胡彦斌',
+    address: '西湖区湖底公园1号',
+    content: [1, 2, 3],
+  },
+  {
+    userId: 9,
+    name: '胡彦斌',
+    address: '西湖区湖底公园1号',
+    content: [1, 2, 3],
+  },
+  {
+    userId: 10,
+    name: '胡彦斌',
+    address: '西湖区湖底公园1号',
+    content: [1, 2, 3],
   },
 ];
+const initialState = {
+  addVisible: false,
+  updVisible: false,
+  loading: false,
+  data: [],
+  pagination: {
+    page: 1,
+    pageSize: 10,
+  },
+};
 
 const columns = [
   {
-    title: '姓名',
+    title: '创建时间',
+    dataIndex: 'content',
+    key: 'content',
+  },
+  {
+    title: '钱包名称',
     dataIndex: 'name',
     key: 'name',
   },
   {
-    title: '年龄',
-    dataIndex: 'age',
-    key: 'age',
-  },
-  {
-    title: '住址',
+    title: '钱包地址',
     dataIndex: 'address',
     key: 'address',
   },
   {
-    title: 'Action',
+    title: '操作',
     key: 'action',
     render: () => (
       <Space>
-        <a>编辑</a>
+        <a>修改密码</a>
         <a>删除</a>
       </Space>
     ),
   },
 ];
 
-export default function WalletManage() {
-  // const [pagination, setPagination] = useState({page: 1, pageSize: 10, total: 14})
-  // const [loading, setLoading] = useState(false)
+type IState = Readonly<typeof initialState>;
 
-  // const tableChangeHandle = (newPagination: Pagination) => {
-  //   console.log('table changed ~', newPagination)
-  // }
+export default class WalletManage extends React.Component<IState> {
+  readonly state: IState = initialState;
 
-  return (
-    <PageHeaderWrapper>
-      <div className="container">
-        <Table
-          rowKey="walletManage"
-          dataSource={dataSource}
-          columns={columns}
-          // pagination={pagination}
-          // loading={loading}
-          // onChange={() => tableChangeHandle(pagination)}
-          bordered
-        />
-        ;
-      </div>
-    </PageHeaderWrapper>
-  );
+  addHandle = (modalType: string) => {
+    console.log(modalType);
+    this.openModal(modalType);
+  };
+
+  tableChangeHandle = (newPagination: any) => {
+    console.log('newPagination', newPagination);
+  };
+
+  openModal = (modalType: string) => {
+    console.log(modalType);
+    this.setState({
+      [modalType]: true,
+    });
+  };
+
+  closeModal = (modalType: string) => {
+    console.log(modalType);
+    this.setState({
+      [modalType]: false,
+    });
+  };
+
+  handleOk = (modalType: string) => {
+    this.closeModal(modalType);
+  };
+
+  handleCancel = (modalType: string) => {
+    this.closeModal(modalType);
+  };
+
+  render() {
+    const { loading, addVisible, updVisible, pagination } = this.state;
+    // const { getFieldProps } = this.props.form
+    return (
+      <PageHeaderWrapper>
+        <div className="container">
+          <Card>
+            <Button
+              type="primary"
+              style={{ float: 'right' }}
+              onClick={() => this.addHandle('addVisible')}
+            >
+              新增
+            </Button>
+          </Card>
+          <Table
+            rowKey={(record) => record.userId}
+            dataSource={dataSource}
+            columns={columns}
+            pagination={pagination}
+            loading={loading}
+            onChange={() => this.tableChangeHandle(pagination)}
+            bordered
+          />
+          <Modal
+            centered
+            title="新增钱包账户"
+            visible={addVisible}
+            onOk={() => this.handleOk('addVisible')}
+            onCancel={() => this.handleCancel('addVisible')}
+          >
+            <div className="modal_container">
+              <Form name="nest-messages" onFinish={(values) => this.handleOk('addVisible', values)}>
+                <Form.Item label="私钥/助记词/keystore文件" name="content">
+                  <Input.TextArea />
+                </Form.Item>
+                <Form.Item label="账户名称" name="name">
+                  <Input />
+                </Form.Item>
+                <Form.Item label="钱包密码" name="password">
+                  <Input type="password" />
+                </Form.Item>
+              </Form>
+            </div>
+          </Modal>
+          <Modal
+            centered
+            title="修改钱包密码"
+            visible={updVisible}
+            onOk={() => this.handleOk('updVisible')}
+            onCancel={() => this.handleCancel('updVisible')}
+          >
+            <div className="modal_container">
+              <Form name="nest-messages" onFinish={(values) => this.handleOk('addVisible', values)}>
+                <Form.Item label="钱包名称">
+                  <div>wallet name</div>
+                </Form.Item>
+                <Form.Item label="原密码" name="password">
+                  <Input type="password" />
+                </Form.Item>
+                <Form.Item label="新密码" name="newPassword">
+                  <Input type="password" />
+                </Form.Item>
+                <Form.Item label="确认密码" name="confirmPassword">
+                  <Input type="password" />
+                </Form.Item>
+              </Form>
+            </div>
+          </Modal>
+        </div>
+      </PageHeaderWrapper>
+    );
+  }
 }
